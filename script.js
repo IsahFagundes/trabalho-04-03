808ad8c7f3a0a927f3ef6d791ebcbeadf057e6fc
IsahFagundes <isabelli.schlogl@escola.pr.gov.br>
2026-04-14 14:52:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function abrirCadastro() {
+    document.getElementById("popup").style.display = "block";
+}
+
+function fecharCadastro() {
+    document.getElementById("popup").style.display = "none";
+}
