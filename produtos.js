efd20aa87deaf36181400faaf943a78263ee22fc
IsahFagundes <isabelli.schlogl@escola.pr.gov.br>
2026-04-15 00:54:30 -0300

    Add files via upload

---
+++ produtos.js
@@ -0,0 +1,90 @@
+const produtos = [
+    //  MASCULINOS 
+    {
+        id: 1,
+        nome: "BLEU DE CHANEL",
+        preco: 560.00,
+        imagem: "img/perfumeM1.jpg",
+        bio: "Perfume masculino elegante com notas cítricas e amadeiradas. Sofisticação e presença marcante."
+    },
+    {
+        id: 2,
+        nome: "SAUVAGE DIOR",
+        preco: 473.20,
+        imagem: "img/perfumeM2.jpg",
+        bio: "Fragrância fresca e intensa com bergamota e ambroxan. Moderno e poderoso."
+    },
+    {
+        id: 3,
+        nome: "GIORGIO ARMANI",
+        preco: 384.98,
+        imagem: "img/perfumeM3.jpg",
+        bio: "Elegante e leve, com notas aquáticas e amadeiradas."
+    },
+    {
+        id: 4,
+        nome: "DOLCE & GABBANA",
+        preco: 788.00,
+        imagem: "img/perfumeM4.jpg",
+        bio: "Perfume quente e sedutor com toque especiado."
+    },
+    {
+        id: 5,
+        nome: "GAULTIER",
+        preco: 827.10,
+        imagem: "img/perfumeM5.jpg",
+        bio: "Fragrância forte e marcante com identidade única."
+    },
+    {
+        id: 6,
+        nome: "GAULTIER CLASSIC",
+        preco: 827.10,
+        imagem: "img/perfumeM6.jpg",
+        bio: "Clássico intenso, doce e sofisticado."
+
+    },
+
+    //  FEMININOS 
+    {
+        id: 7,
+        nome: "MISS DIOR",
+        preco: 639.00,
+        imagem: "img/perfume.jpg",
+        bio: "Perfume feminino floral elegante. Romântico e sofisticado, perfeito para ocasiões especiais."
+    },
+    {
+        id: 8,
+        nome: "COCO CHANEL",
+        preco: 815.00,
+        imagem: "img/perfume1.jpg",
+        bio: "Fragrância luxuosa com notas orientais e florais. Clássico e atemporal."
+    },
+    {
+        id: 9,
+        nome: "CAROLINA HERRERA",
+        preco: 545.00,
+        imagem: "img/perfume2.jpg",
+        bio: "Perfume moderno e doce com toque sedutor e elegante."
+    },
+    {
+        id: 10,
+        nome: "CHANCE CHANEL",
+        preco: 937.50,
+        imagem: "img/perfume3.jpg",
+        bio: "Fragrância leve e vibrante com notas florais e frescas."
+    },
+    {
+        id: 11,
+        nome: "KAY ALI",
+        preco: 259.00,
+        imagem: "img/perfume5.jpg",
+        bio: "Perfume doce e intenso, moderno e marcante."
+    },
+    {
+        id: 12,
+        nome: "KAY ALI INTENSE",
+        preco: 325.10,
+        imagem: "img/perfume6.jpg",
+        bio: "Versão mais forte e envolvente, perfeita para noites especiais."
+    }
+];
--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function abrirCadastro() {
 function fecharCadastro() {
     document.getElementById("popup").style.display = "none";
 }
+
+function cadastrar() {
+    let nome = document.getElementById("nome").value;
+
+    if(nome === "") {
+        document.getElementById("mensagem").innerText = "Digite seu nome!";
+    } else {
+        document.getElementById("mensagem").innerText = 
+            "Seja bem-vindo, " + nome + "!";
+    }
+}
